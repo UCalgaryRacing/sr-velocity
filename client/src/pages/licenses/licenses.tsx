6afbcbde1d9fc9a9297a50bd3b1502f3fef51251
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-10-23 14:51:15 -0600

    Licenses page and CSS

--- client/src/pages/licenses/licenses.tsx
+++ client/src/pages/licenses/licenses.tsx
@@ -1,7 +1,26 @@
 import React from "react";
 
+import "./licenses.css";
+import licenses from "./licenses.json";
+
 const Licenses: React.FC = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Licenses</h1>
+      {licenses.map((data, key) => {
+        return (
+          <div key={key} className="content">
+            <h4>{data.name}</h4>
+            <p>
+              {data.copyright}
+              <br></br>
+              <a href={data.repository}>{data.licenses}</a>
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Licenses;
