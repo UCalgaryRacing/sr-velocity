60112b95e250bcbd8f9c78dbb2437fbc7868a0c1
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-10-23 19:45:14 -0600

    Add requested changes

--- client/src/pages/licenses/licenses.py
+++ client/src/pages/licenses/licenses.py
@@ -4,9 +4,23 @@ import re
 import subprocess
 
 
+def format_repo(repo: str):
+    repo = repo.removeprefix("git+")
+
+    # Truncate path to base repo path
+    if repo.count("/") > 4:
+        repo = repo.split("/")
+        repo = "/".join(repo[1:4])
+        repo = repo.removeprefix("/")
+        repo = "https://" + repo
+
+    return repo
+
+
 def format_copyright(copyright: str):
     # Format (c) to unicode copyright
     copyright = copyright.replace("(c)", "\u00A9").replace("(C)", "\u00A9")
+
     copyright = copyright.replace(".", "")
 
     # Truncate after "All rights reserved". Lowercase to handle weird case
@@ -17,8 +31,12 @@ def format_copyright(copyright: str):
     # Remove site links
     copyright = " ".join([t for t in copyright.split() if not t.startswith("http")])
 
-    # Replace bracket errors with brackets, then remove all text withing brackets
+    # Replace bracket errors with brackets
     copyright = copyright.replace("&lt;", "<").replace("&gt;", ">")
+    # Regex pattern is brackets with any number of non-bracket characters inside the surrounding brackets
+    # 3 patterns for 3 different bracket types
+    # Used mainly to remove emails and links from copyright messages while leaving names
+    # e.g Foo Bar <foo.bar@baz.com> or (github.com/foo)
     copyright = re.sub(r"\([^()]*\)", "", copyright)
     copyright = re.sub(r"\<[^<>]*\>", "", copyright)
     copyright = re.sub(r"\[[^\[\]]*\]", "", copyright)
@@ -26,10 +44,10 @@ def format_copyright(copyright: str):
     return copyright
 
 
-# Enables program to work from root sr-velocity folder
-cwd = os.getcwd()
-if not cwd.endswith("client") and not cwd.endswith("client/"):
-    os.chdir("client")
+# Set working directory to script location
+abspath = os.path.abspath(__file__)
+dir_name = os.path.dirname(abspath)
+os.chdir(dir_name)
 
 # Runs license-checker to update package info
 subprocess.run(
@@ -38,13 +56,15 @@ subprocess.run(
         "license-checker",
         "--json",
         "--out",
-        "src/pages/licenses/licenses.json",
+        "licenses.json",
         "--customPath",
-        "src/pages/licenses/licenses_format.json",
+        "licenses_format.json",
+        "--start",
+        "../../../",  # Starts search from client folder, 3 levels up from cwd
     ]
 )
 
-with open("src/pages/licenses/licenses.json") as f:
+with open("licenses.json") as f:
     package_dict: dict[str, dict[str, str]] = json.load(f)
 
 for package in package_dict.items():
@@ -66,19 +86,22 @@ for package in dirty_packages:
     repo = package.get("repository")
     name = package["name"]
 
+    # Skip package if it is a duplicate
+    if name in seen_names:
+        continue
+
     # Skip packages with no repository or with invalid URL
     if repo is None or repo.startswith("git:"):
         continue
 
-    # Format repo URL
-    repo = repo.removeprefix("git+")
-    if repo.count("/") > 4:
-        repo = repo.split("/")
-        repo = "/".join(repo[1:4])
-        repo = repo.removeprefix("/")
-        repo = "https://" + repo
-    package["repository"] = repo  # Update repo URL in dict
+    repo = format_repo(repo)
+    # Skip package if it is a duplicate
+    if repo in seen_repos:
+        continue
+    seen_repos.add(repo)
+    seen_names.add(name)
 
+    package["repository"] = repo
     package["copyright"] = format_copyright(package["copyright"])
 
     # Set copyright to publisher if copyright wasn't available
@@ -86,22 +109,12 @@ for package in dirty_packages:
         package["copyright"] = "Copyright \u00A9 " + package["publisher"]
 
     # Remove unnecessary keys from package dict
-    if package.get("publisher") is not None:
-        del package["publisher"]
-    if package.get("email") is not None:
-        del package["email"]
-    if package.get("path") is not None:
-        del package["path"]
-    if package.get("licenseFile") is not None:
-        del package["licenseFile"]
-    if package.get("licenseText") is not None:
-        del package["licenseText"]
+    for key in ["publisher", "email", "path", "licenseFile", "licenseText"]:
+        package.pop(key, None)
 
-    # Add only non-duplicate packages
-    if repo not in seen_repos and name not in seen_names:
-        packages.append(package)
-        seen_repos.add(repo)
-        seen_names.add(name)
+    packages.append(package)
 
-with open("src/pages/licenses/licenses.json", "w") as f:
+
+with open("licenses.json", "w") as f:
     f.write(json.dumps(packages, indent=2))
+
