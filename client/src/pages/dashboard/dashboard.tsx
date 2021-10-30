4107a316332c9a79afdba870817b13ff847e297b
ryepain <ryan.painchaud@ucalgary.ca>
2021-10-30 16:04:19 -0600

    Formatting + Styling 2

--- client/src/pages/dashboard/dashboard.tsx
+++ client/src/pages/dashboard/dashboard.tsx
@@ -1,60 +1,80 @@
 import React, { useState } from "react";
-import { ProSidebar, Menu, MenuItem, SubMenu, SidebarHeader } from "react-pro-sidebar";
+import {
+  ProSidebar,
+  Menu,
+  MenuItem,
+  SubMenu,
+  SidebarHeader,
+} from "react-pro-sidebar";
 import "react-pro-sidebar/dist/css/styles.css";
 import "./dashboard.css";
-import Hamburger from "hamburger-react"
+import Hamburger from "hamburger-react";
 
 // <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
 
 const Dashboard: React.FC = () => {
   const [title, setTitle] = useState("Streaming - A");
   const [collapsed, setCollapsed] = useState(false);
-  const [isOpen, setOpen] = useState(true)
+  const [isOpen, setOpen] = useState(true);
 
   return (
-    <div id='dashboard'>
-      <ProSidebar collapsed={collapsed} collapsedWidth={75}>
-        
-        <SidebarHeader> <Hamburger onToggle={() => {
-          setCollapsed(!collapsed)
-          }} toggled={isOpen} toggle={setOpen}></Hamburger> 
-          </SidebarHeader>
-        
-        <Menu iconShape="square">
+    <div id="dashboard">
+      <ProSidebar collapsed={collapsed} collapsedWidth={75} breakPoint={"md"}>
+        <SidebarHeader>
+          <Hamburger
+            onToggle={() => {
+              setCollapsed(!collapsed);
+            }}
+            toggled={isOpen}
+            toggle={setOpen}
+          />
+        </SidebarHeader>
 
-          <SubMenu defaultOpen={true} title="Streaming" icon>
-            <MenuItem onClick={() => {
-              setTitle("Streaming - A")
-            }}>A</MenuItem>
-            <MenuItem onClick={() => {
-              setTitle("Streaming - B")
-            }} >B</MenuItem>
-            <MenuItem onClick={() => {
-              setTitle("Streaming - C")
-            }}>C</MenuItem>
+        <Menu>
+          <SubMenu title="Streaming" defaultOpen={true}>
+            <MenuItem
+              onClick={() => {
+                setTitle("Streaming - A");
+              }}
+            >
+              A
+            </MenuItem>
+            <MenuItem
+              onClick={() => {
+                setTitle("Streaming - B");
+              }}
+            >
+              B
+            </MenuItem>
+            <MenuItem
+              onClick={() => {
+                setTitle("Streaming - C");
+              }}
+            >
+              C
+            </MenuItem>
           </SubMenu>
 
-          <SubMenu title="Historical" icon>
+          <SubMenu title="Historical">
             <MenuItem>A</MenuItem>
             <MenuItem>B</MenuItem>
             <MenuItem>C</MenuItem>
           </SubMenu>
 
-          <SubMenu title="Manage" icon>
-          <MenuItem>A</MenuItem>
-          <MenuItem>B</MenuItem>
-          <MenuItem>C</MenuItem>
+          <SubMenu title="Manage">
+            <MenuItem>A</MenuItem>
+            <MenuItem>B</MenuItem>
+            <MenuItem>C</MenuItem>
           </SubMenu>
 
-          <SubMenu title="Sensor Fusion" icon>
-          <MenuItem>A</MenuItem>
-          <MenuItem>B</MenuItem>
-          <MenuItem>C</MenuItem>
+          <SubMenu title="Sensor Fusion">
+            <MenuItem>A</MenuItem>
+            <MenuItem>B</MenuItem>
+            <MenuItem>C</MenuItem>
           </SubMenu>
-
         </Menu>
       </ProSidebar>
-      <div id='content'>{title}</div>
+      <div id="content">{title}</div>
     </div>
   );
 };
