63aaa0f3f7ebe0ad6e02c438f9889214a636df61
Tijunel <justintijunel@gmail.com>
2021-10-30 19:12:24 -0600

    Regex solution to backend web page serving

--- backend/server.js
+++ backend/server.js
@@ -1,76 +1,59 @@
 "use strict";
+
 // Set up env
-require('dotenv').config()
+require("dotenv").config();
 
 // Library Imports
 const express = require("express");
 const app = express();
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const path = require('path');
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const path = require("path");
 
-const io = require('socket.io')(process.env.REACT_SOCKETIO_PORT);
-const socketIOClient = require('socket.io-client');
+const io = require("socket.io")(process.env.REACT_SOCKETIO_PORT);
+const socketIOClient = require("socket.io-client");
 
 // SocketIO
-const socket = socketIOClient('http://' + process.env.IOT_MS_IP + ':' + process.env.DATA_API_SOCKETIO, {
-  //CHANGE WHEN DEPLOYING!
-  reconnection: true,
-});
+const socket = socketIOClient(
+  "http://" + process.env.DATA_API_MS_IP + ":" + process.env.DATA_API_SOCKETIO,
+  {
+    reconnection: true,
+  }
+);
 
 // Forward incoming data to React
-socket.on('new data', (data) => {
-    io.emit(data);
-  });
-  
+socket.on("new data", (data) => {
+  io.emit(data);
+});
 
 // Express Setup
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
-    session({
-      secret: 'xCufvwEyu14Tuu7l',
-      resave: true,
-      saveUninitialized: true,
-      secure: true,
-    })
+  session({
+    secret: "xCufvwEyu14Tuu7l",
+    resave: true,
+    saveUninitialized: true,
+    secure: true,
+  })
 );
 app.use(cookieParser());
 
-// What the fuck (just copy pasted from last one lol)
-app.use(express.static(path.join(__dirname, 'client/build')));
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
-app.get('/streaming', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
-app.get('/historical', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
-app.get('/manage', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
-app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
-app.get('/signin', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
-app.get('/signup', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
-app.get('/licenses', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+// All routes except those starting with /api/ should serve web pages
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
+app.get(new RegExp("(?!/api/).+"), (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
 });
 
 // Import Routes
-const sensor = require('./routes/sensor')
+const sensor = require("./routes/sensor");
 
 // Setup Routes
-app.use('/sensor', sensor)
+app.use("/sensor", sensor);
 
 // Begin Server
-app.listen(process.env.GATEWAY_PORT, () => console.log(`Listening on port ${process.env.GATEWAY_PORT}`));
+app.listen(process.env.GATEWAY_PORT, () =>
+  console.log(`Listening on port ${process.env.GATEWAY_PORT}`)
+);
