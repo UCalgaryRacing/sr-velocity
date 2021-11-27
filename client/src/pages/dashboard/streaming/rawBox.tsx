81871fb317395a26b8a71485b6f219b0b2580ded
ryepain <ryan.painchaud@ucalgary.ca>
2021-11-27 14:38:26 -0700

    Random Numbers (in progress)

--- client/src/pages/dashboard/streaming/rawBox.tsx
+++ client/src/pages/dashboard/streaming/rawBox.tsx
@@ -1,18 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./rawBox.css";
+
+var rn = require("random-number");
 
 interface RawBoxProps {
   sensor: any;
 }
 
+const a = "red";
+
 const RawBox: React.FC<RawBoxProps> = (props: RawBoxProps) => {
-  console.log(props.sensor.name);
+  const [value, setValue] = useState(0);
+
+  var optionsLow = {
+    min: 0,
+    max: 5,
+    integer: true,
+  };
+
+  var optionsHigh = {
+    min: 10,
+    max: 15,
+    integer: true,
+  };
+
+  var upperBound = rn(optionsHigh);
+  var lowerBound = rn(optionsLow);
+
+  var optionsValue = {
+    min: lowerBound,
+    max: upperBound,
+    integer: true,
+  };
+
+  // setValue(rn(optionsValue));
+  const rvalue = rn(optionsValue);
+
   return (
-    <div className="raw-box">
+    <div className="raw-box" style={{ backgroundColor: a }}>
       {props.sensor.name}
       <br />
       <br />
-      {props.sensor.value}
+      {rvalue}
+      {/* {props.sensor.value} */}
     </div>
   );
 };
