576f29070a6946d23f5b4c67d01cd412f58fb224
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-02-04 18:18:28 -0700

    Handle fetching and listing sessions

--- client/src/pages/dashboard/historical/data.tsx
+++ client/src/pages/dashboard/historical/data.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
+import SessionList, { Session } from "./sessionList";
+import { useFetch } from "../../../hooks/useFetch";
+
 const Data: React.FC = () => {
-  return <div id="data">Data</div>;
+  // Temporary URL, using json-server for dummy data
+  const { data: sessions, error } = useFetch<Session[]>("http://localhost:3001/sessions");
+
+  if (error) return <p>Error fetching data</p>;
+  if (!sessions) return <p>Loading...</p>;
+  return (
+    <div id="data">
+      <SessionList sessions={sessions} />
+    </div>
+  );
 };
 
 export default Data;
