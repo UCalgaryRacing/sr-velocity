dee69fcd5d923171c7f853afb4a98fd783c2abae
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-03-12 13:00:44 -0700

    Update naming and add copyright

--- client/src/hooks/useFetch.ts
+++ client/src/hooks/useFetch.ts
@@ -21,14 +21,14 @@ export function useFetch<T = unknown>(url?: string, options?: RequestInit): Stat
 
   const fetchReducer = (state: State<T>, action: Action<T>): State<T> => {
     switch (action.type) {
-      case "loading":
-        return { ...initialState };
-      case "fetched":
-        return { ...initialState, data: action.payload };
-      case "error":
-        return { ...initialState, error: action.payload };
-      default:
-        return state;
+    case "loading":
+      return { ...initialState };
+    case "fetched":
+      return { ...initialState, data: action.payload };
+    case "error":
+      return { ...initialState, error: action.payload };
+    default:
+      return state;
     }
   };
 
