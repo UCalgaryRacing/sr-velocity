4b2576ced63510c9d44be8db859528a3da2b7a5e
Tijunel <justintijunel@gmail.com>
2022-03-16 15:12:53 -0600

    Minor updates

---
+++ client/src/crud/rawDataPresets.ts
@@ -0,0 +1,20 @@
+// Copyright Schulich Racing, FSAE
+// Written by Justin Tijunelis
+
+import { RawDataPreset } from "state";
+
+export const getRawDataPresets = (thingId: string) => {
+  return new Promise<RawDataPreset[]>((resolve, reject) => {});
+};
+
+export const postRawDataPreset = (rawDataPreset: RawDataPreset) => {
+  return new Promise<void>((resolve, reject) => {});
+};
+
+export const putRawDataPreset = (rawDataPreset: RawDataPreset) => {
+  return new Promise<void>((resolve, reject) => {});
+};
+
+export const deleteRawDataPreset = (rawDataPresetId: string) => {
+  return new Promise<void>((resolve, reject) => {});
+};
---
+++ client/src/state/types/rawDataPreset.ts
@@ -0,0 +1,9 @@
+// Copyright Schulich Racing FSAE
+// Written by Justin Tijunelis
+
+export type RawDataPreset = {
+  _id: string;
+  thing_id: string;
+  name: string;
+  sensors: string[];
+};
