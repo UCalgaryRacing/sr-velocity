eb8c6a2944fcbdd8d83fbe47a6e21cb53893252a
Jeremy Bilic <jeremy.bilic@gmail.com>
2022-03-26 14:18:33 -0600

    Added skeleton

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,4 +1,32 @@
-async function withAnyAuth(req, res, next) {
+/*
+* Calls the next function if the auth is the required one or higher
+*/
+
+permissionValues = {
+  Pending: 1,
+  Guest: 2,
+  Member: 3,
+  Lead: 4,
+  Admin: 5,
+}
+
+async function hasAuth(perms, required_perms) {
+  return permissionValues[perms] >= permissionValues[required_perms]
+}
+
+async function withPendingAuth(req, res, next) {
+  next()
+}
+
+async function withGuestAuth(req, res, next) {
+  next()
+}
+
+async function withMemberAuth(req, res, next) {
+  next();
+}
+
+async function withLeadsAuth(req, res, next) {
   next();
 }
 
@@ -6,8 +34,5 @@ async function withAdminAuth(req, res, next) {
   next();
 }
 
-async function withCaptainAuth(req, res, next) {
-  next();
-}
 
-module.exports = { withAnyAuth, withAdminAuth, withCaptainAuth };
+module.exports = { withPendingAuth, withGuestAuth, withMemberAuth, withLeadsAuth, withAdminAuth };
