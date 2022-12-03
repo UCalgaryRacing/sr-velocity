59dc5b97a4750c7815565d6b620d8330c3097e81
Hamza Amar <rhamzax@gmail.com>
2022-12-03 02:41:53 -0700

    Use built in components instead

--- client/src/pages/request/request.tsx
+++ client/src/pages/request/request.tsx
@@ -1,31 +1,58 @@
+import { InputField, TextArea, TextButton } from "components/interface";
 import React from "react";
 import { useState } from 'react';
-
+import { useForm } from "hooks";
 const Request = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [values, handleChange] = useForm({
+    email: "",
+    name: "",
+    phoneNumber: "",
+    inquiry: "",
+  });
+  
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>):void => {
+    event.preventDefault();
+    console.log(values);
+  }
 
-  const[name, setName] = useState("")
-  const[email, setEmail] = useState("")
-  const[inquiry, setInquiry] = useState("")
-
-  // const handleSubmit = () => {
-  //   pass
-  // }
   return (
-    <form >
-      <label>
-        Name:
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
-      </label>
-      <label>
-        Email:
-        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)}/>
-      </label>
-      <label>
-        Inquiry:
-        <textarea placeholder="Enter question here" value={inquiry} onChange={(e) => setInquiry(e.target.value)}/>
-      </label>
-      <button type="submit">Submit</button>
-    </form>
+      <div className="flex flex-col mt-24 justify-center items-center container">
+        <img src="assets/team-logo.svg" className="max-w-[400px]"/>
+        <h1>Contact Us</h1>
+        <form onSubmit={handleSubmit} className="grid grid-cols-1 min-w-fit gap-8">
+          <InputField
+          name="name"
+          type="name"
+          title="Name"
+          value={values.name}
+          onChange={handleChange}
+          required
+          />
+          <InputField
+          name="email"
+          type="email"
+          title="Email"
+          value={values.email}
+          onChange={handleChange}
+          required
+          />
+          <InputField
+          name="phoneNumber"
+          type="phoneNumber"
+          title="Phone Number"
+          value={values.phoneNumber}
+          onChange={handleChange}
+          required
+          />
+          <TextArea
+          holder="Inquiry"
+          value={values.inquiry}
+          onUpdate={handleChange}
+          />
+          <TextButton title="Send" loading={loading} />
+        </form>
+      </div> 
   )
 }
 
